39c300c4ac0d13c7bb0fdec532a3c851ca66f1be
levistieber <72553310+levistieber@users.noreply.github.com>
2021-12-09 21:28:48 +0100

    Add files via upload

--- LeventEvolution/script/scriptOneP.js
+++ LeventEvolution/script/scriptOneP.js
@@ -1,11 +1,11 @@
-var canvas1 = $('#canvas1')[0];
-var ctx1 = canvas1.getContext('2d');
-var scoreLabel1 = $('#score1')[0];
-var score1 = 0;
+var canvas = $('#canvas')[0];
+var ctx = canvas.getContext('2d');
+var scoreLabel = $('#score')[0];
+var score = 0;
 var size = 4;
-var width = canvas1.width / size - 6;
-var cells1 = [];
-var loss1 = false;
+var width = canvas.width / size - 6;
+var cells = [];
+var loss = false;
 var img1 = $("#first")[0];
 var img2 = $("#second")[0];
 var img3 = $("#third")[0];
@@ -27,163 +27,163 @@ function cell(row, coll) {
   this.y = row * width + 5 * (row + 1);
 }
 
-function createCells1() {
+function createCells() {
   var i, j;
   for(i = 0; i < size; i++) {
-    cells1[i] = [];
+    cells[i] = [];
     for(j = 0; j < size; j++) {
-      cells1[i][j] = new cell(i, j);
+      cells[i][j] = new cell(i, j);
     }
   }
 }
 
 
-function drawCell1(cell) {
-  ctx1.beginPath();
-  var pat1 = ctx1.createPattern(img1, "repeat");
-  var pat2 = ctx1.createPattern(img2, "repeat");
-  var pat3 = ctx1.createPattern(img3, "repeat");
-  var pat4 = ctx1.createPattern(img4, "repeat");
-  var pat5 = ctx1.createPattern(img5, "repeat");
-  var pat6 = ctx1.createPattern(img6, "repeat");
-  var pat7 = ctx1.createPattern(img7, "repeat");
-  var pat8 = ctx1.createPattern(img8, "repeat");
-  var pat9 = ctx1.createPattern(img9, "repeat");
-  var pat10 = ctx1.createPattern(img10, "repeat");
-  var pat11 = ctx1.createPattern(img11, "repeat");
-  var pat12 = ctx1.createPattern(img12, "repeat");
+function drawCell(cell) {
+  ctx.beginPath();
+  var pat1 = ctx.createPattern(img1, "repeat");
+  var pat2 = ctx.createPattern(img2, "repeat");
+  var pat3 = ctx.createPattern(img3, "repeat");
+  var pat4 = ctx.createPattern(img4, "repeat");
+  var pat5 = ctx.createPattern(img5, "repeat");
+  var pat6 = ctx.createPattern(img6, "repeat");
+  var pat7 = ctx.createPattern(img7, "repeat");
+  var pat8 = ctx.createPattern(img8, "repeat");
+  var pat9 = ctx.createPattern(img9, "repeat");
+  var pat10 = ctx.createPattern(img10, "repeat");
+  var pat11 = ctx.createPattern(img11, "repeat");
+  var pat12 = ctx.createPattern(img12, "repeat");
 
-  ctx1.rect(cell.x, cell.y, width, width);
+  ctx.rect(cell.x, cell.y, width, width);
   switch (cell.value) {
     case 0 :
-      ctx1.fillStyle = '#A9A9A9';
+      ctx.fillStyle = '#A9A9A9';
       break;
     case 2 :
-      ctx1.fillStyle = pat1;
+      ctx.fillStyle = pat1;
       break;
     case 4 :
-      ctx1.fillStyle = pat2;
+      ctx.fillStyle = pat2;
       break;
     case 8 :
-      ctx1.fillStyle = pat3;
+      ctx.fillStyle = pat3;
       break;
     case 16 :
-      ctx1.fillStyle = pat4;
+      ctx.fillStyle = pat4;
       break;
     case 32 :
-      ctx1.fillStyle = pat5;
+      ctx.fillStyle = pat5;
       break;
     case 64 :
-      ctx1.fillStyle = pat6;
+      ctx.fillStyle = pat6;
       break;
     case 128 :
-      ctx1.fillStyle = pat7;
+      ctx.fillStyle = pat7;
       break;
     case 256 :
-      ctx1.fillStyle = pat8;
+      ctx.fillStyle = pat8;
       break;
     case 512 :
-      ctx1.fillStyle = pat9;
+      ctx.fillStyle = pat9;
       break;
     case 1024 :
-      ctx1.fillStyle = pat10;
+      ctx.fillStyle = pat10;
       break;
     case 2048 :
-      ctx1.fillStyle = pat11;
+      ctx.fillStyle = pat11;
       break;
     case 4096 :
-      ctx1.fillStyle = pat12;
+      ctx.fillStyle = pat12;
       break;
     default :
-      ctx1.fillStyle = '#ff0080';
+      ctx.fillStyle = '#ff0080';
   }
-  ctx1.fill();
+  ctx.fill();
 }
 
 document.onkeydown = function (event) {
-  if (!loss1) {
+  if (!loss) {
     if (event.keyCode === 87 || event.keyCode === 38) {
-      moveUp1();
+      moveUp();
     } else if (event.keyCode === 68 || event.keyCode === 39) {
-      moveRight1();
+      moveRight();
     } else if (event.keyCode === 83 || event.keyCode === 40) {
-      moveDown1();
+      moveDown();
     } else if (event.keyCode === 65 || event.keyCode === 37) {
-      moveLeft1();
+      moveLeft();
     }
   }
-  if (loss1 === true) {
-      window.alert("Game over! Your points: " + score1);
+  if (loss === true) {
+      window.alert("Game over! Your points: " + score);
     }
-  scoreLabel1.innerHTML = score1 + ' points';
+  scoreLabel.innerHTML = score + ' points';
 }
 
 function startGame() {
-  createCells1();
-  drawAllCells1();
-  pasteNewCell1();
-  pasteNewCell1();
+  createCells();
+  drawAllCells();
+  pasteNewCell();
+  pasteNewCell();
   hidePics();
 }
 
-function finishGame1() {
-  canvas1.style.opacity = '0.5';
-  loss1 = true;
+function finishGame() {
+  canvas.style.opacity = '0.5';
+  loss = true;
 }
 
 
-function drawAllCells1() {
+function drawAllCells() {
   var i, j;
   for(i = 0; i < size; i++) {
     for(j = 0; j < size; j++) {
-      drawCell1(cells1[i][j]);
+      drawCell(cells[i][j]);
     }
   }
 }
 
 
-function pasteNewCell1() {
+function pasteNewCell() {
   var countFree = 0;
   var i, j;
   for(i = 0; i < size; i++) {
     for(j = 0; j < size; j++) {
-      if(!cells1[i][j].value) {
+      if(!cells[i][j].value) {
         countFree++;
       }
     }
   }
   if(!countFree) {
-    finishGame1();
+    finishGame();
     return;
   }
   while(true) {
     var row = Math.floor(Math.random() * size);
     var coll = Math.floor(Math.random() * size);
-    if(!cells1[row][coll].value) {
-      cells1[row][coll].value = 2 * Math.ceil(Math.random() * 2);
-      drawAllCells1();
+    if(!cells[row][coll].value) {
+      cells[row][coll].value = 2 * Math.ceil(Math.random() * 2);
+      drawAllCells();
       return;
     }
   }
 }
 
 
-function moveRight1() {
+function moveRight() {
   var i, j;
   var coll;
   for(i = 0; i < size; i++) {
     for(j = size - 2; j >= 0; j--) {
-      if(cells1[i][j].value) {
+      if(cells[i][j].value) {
         coll = j;
         while (coll + 1 < size) {
-          if (!cells1[i][coll + 1].value) {
-            cells1[i][coll + 1].value = cells1[i][coll].value;
-            cells1[i][coll].value = 0;
+          if (!cells[i][coll + 1].value) {
+            cells[i][coll + 1].value = cells[i][coll].value;
+            cells[i][coll].value = 0;
             coll++;
-          } else if (cells1[i][coll].value == cells1[i][coll + 1].value) {
-            cells1[i][coll + 1].value *= 2;
-            score1 +=  cells1[i][coll + 1].value;
-            cells1[i][coll].value = 0;
+          } else if (cells[i][coll].value == cells[i][coll + 1].value) {
+            cells[i][coll + 1].value *= 2;
+            score +=  cells[i][coll + 1].value;
+            cells[i][coll].value = 0;
             break;
           } else {
             break;
@@ -192,25 +192,25 @@ function moveRight1() {
       }
     }
   }
-  pasteNewCell1();
+  pasteNewCell();
 }
 
-function moveLeft1() {
+function moveLeft() {
   var i, j;
   var coll;
   for (i = 0; i < size; i++) {
     for (j = 1; j < size; j++) {
-      if (cells1[i][j].value) {
+      if (cells[i][j].value) {
         coll = j;
         while (coll - 1 >= 0) {
-          if (!cells1[i][coll - 1].value) {
-            cells1[i][coll - 1].value = cells1[i][coll].value;
-            cells1[i][coll].value = 0;
+          if (!cells[i][coll - 1].value) {
+            cells[i][coll - 1].value = cells[i][coll].value;
+            cells[i][coll].value = 0;
             coll--;
-          } else if (cells1[i][coll].value == cells1[i][coll - 1].value) {
-            cells1[i][coll - 1].value *= 2;
-            score1 += cells1[i][coll - 1].value;
-            cells1[i][coll].value = 0;
+          } else if (cells[i][coll].value == cells[i][coll - 1].value) {
+            cells[i][coll - 1].value *= 2;
+            score += cells[i][coll - 1].value;
+            cells[i][coll].value = 0;
             break;
           } else {
             break;
@@ -219,24 +219,24 @@ function moveLeft1() {
       }
     }
   }
-  pasteNewCell1();
+  pasteNewCell();
 }
 
-function moveUp1() {
+function moveUp() {
   var i, j, row;
   for (j = 0; j < size; j++) {
     for (i = 1; i < size; i++) {
-      if (cells1[i][j].value) {
+      if (cells[i][j].value) {
         row = i;
         while (row > 0) {
-          if (!cells1[row - 1][j].value) {
-            cells1[row - 1][j].value = cells1[row][j].value;
-            cells1[row][j].value = 0;
+          if (!cells[row - 1][j].value) {
+            cells[row - 1][j].value = cells[row][j].value;
+            cells[row][j].value = 0;
             row--;
-          } else if (cells1[row][j].value == cells1[row - 1][j].value) {
-            cells1[row - 1][j].value *= 2;
-            score1 += cells1[row - 1][j].value;
-            cells1[row][j].value = 0;
+          } else if (cells[row][j].value == cells[row - 1][j].value) {
+            cells[row - 1][j].value *= 2;
+            score += cells[row - 1][j].value;
+            cells[row][j].value = 0;
             break;
           } else {
             break;
@@ -245,24 +245,24 @@ function moveUp1() {
       }
     }
   }
-  pasteNewCell1();
+  pasteNewCell();
 }
 
-function moveDown1() {
+function moveDown() {
   var i, j, row;
   for (j = 0; j < size; j++) {
     for (i = size - 2; i >= 0; i--) {
-      if (cells1[i][j].value) {
+      if (cells[i][j].value) {
         row = i;
         while (row + 1 < size) {
-          if (!cells1[row + 1][j].value) {
-            cells1[row + 1][j].value = cells1[row][j].value;
-            cells1[row][j].value = 0;
+          if (!cells[row + 1][j].value) {
+            cells[row + 1][j].value = cells[row][j].value;
+            cells[row][j].value = 0;
             row++;
-          } else if (cells1[row][j].value == cells1[row + 1][j].value) {
-            cells1[row + 1][j].value *= 2;
-            score1 += cells1[row + 1][j].value;
-            cells1[row][j].value = 0;
+          } else if (cells[row][j].value == cells[row + 1][j].value) {
+            cells[row + 1][j].value *= 2;
+            score += cells[row + 1][j].value;
+            cells[row][j].value = 0;
             break;
           } else {
             break;
@@ -271,7 +271,7 @@ function moveDown1() {
       }
     }
   }
-  pasteNewCell1();
+  pasteNewCell();
 }
 
 
